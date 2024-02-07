30a1fac4355e26ff9ed4aebc73db8334a6c73ee3
Brenobn <brenobragabn@gmail.com>
2024-02-07 13:42:37 -0300

    Importing custom fonts

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,10 @@
 import { View, Text } from "react-native"
+import { 
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter"
 
 export default function Home() {
   return(
