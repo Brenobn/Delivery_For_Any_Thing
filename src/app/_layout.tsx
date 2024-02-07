731dc8609067413a5a2ac01d40e06da71d9469c7
Brenobn <brenobragabn@gmail.com>
2024-02-07 14:50:11 -0300

    Importing custom fonts

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,5 +1,11 @@
 import { SafeAreaView } from "react-native"
 import { Slot } from "expo-router"
+import { 
+  Inter_400Regular,
+  Inter_500Medium,
+  Inter_600SemiBold,
+  Inter_700Bold,
+} from "@expo-google-fonts/inter" 
 
 export default function Layout() {
   return(
