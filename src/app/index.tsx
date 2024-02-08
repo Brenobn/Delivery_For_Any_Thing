e4e5d3a01c3a51fc10c35e369d537e97968a8723
Brenobn <brenobragabn@gmail.com>
2024-02-07 23:27:04 -0300

    Importing products MENU. SectionList, Text from React. Creating a SectionList

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import { View, FlatList } from "react-native"
+import { View, FlatList, SectionList, Text } from "react-native"
 
-import { CATEGORIES } from "@/utils/data/products"
+import { CATEGORIES, MENU } from "@/utils/data/products"
 
 import { Header } from "@/components/header"
 import { CategoryButton } from "@/components/category-button";
@@ -30,6 +30,15 @@ export default function Home() {
         className="max-h-10 mt-5"
         showsHorizontalScrollIndicator={false}
         contentContainerStyle={{ gap: 12, paddingHorizontal: 20 }}
+      />
+
+      <SectionList 
+        sections={MENU}
+        keyExtractor={(item) => item.id}
+        stickySectionHeadersEnabled={false}
+        renderItem={({ item }) => (
+          <Text>{item.title}</Text>
+        )}
       />
     </View>
   );
