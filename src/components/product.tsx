8e5c4d0e658dc56d35a673e3c7dcbc4b36b6b8f6
Brenobn <brenobragabn@gmail.com>
2024-02-08 00:04:32 -0300

    Importing View, Text from React. Styling View and Text

--- src/components/product.tsx
+++ src/components/product.tsx
@@ -1,4 +1,4 @@
-import { TouchableOpacity, TouchableOpacityProps, Image, ImageProps } from "react-native";
+import { TouchableOpacity, TouchableOpacityProps, Image, ImageProps, View, Text } from "react-native";
 
 type ProductDataProps = {
   title: string
@@ -16,6 +16,11 @@ export function Product({data, ...rest}: ProductProps) {
       {...rest}
     >
       <Image source={data.thumbnail} className="w-20 h-20 rounded-md" />
+
+      <View className="flex-1 ml-3">
+        <Text className="text-slate-100 font-subtitle text-base flex-1">{data.title}</Text>
+        <Text className="text-slate-400 text-xs leading-5 mt-0.5">{data.description}</Text>
+      </View>
     </TouchableOpacity>
   )
 }
