a90e04129d6d61041d3c13845be418492ff77365
Brenobn <brenobragabn@gmail.com>
2024-02-08 00:06:22 -0300

    Importing useRef from React

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { View, FlatList, SectionList, Text } from "react-native"
 
 import { CATEGORIES, MENU } from "@/utils/data/products"
