e9ff094a9e1eb7fb74f6a55d3dbeb0db4e8caaeb
Brenobn <brenobragabn@gmail.com>
2024-02-08 13:57:36 -0300

    Adding Text and styling it, passing the product title

--- src/app/product/[id].tsx
+++ src/app/product/[id].tsx
@@ -31,8 +31,9 @@ export default function Product(){
   return(
     <View className="flex-1">
       <Image source={product.cover} className="w-full h-52" resizeMode="cover"/>
-     
+
       <View className="p-5 mt-8 flex-1">
+        <Text className="text-white text-xl font-heading">{product.title}</Text>
         <Text className="text-lime-400 text-2xl font-heading my-2">
           {formatCurrency(product.price)}
         </Text>
