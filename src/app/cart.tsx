4ab06fde3eb1100ef7288b18c3cacceaaf2c39f8
Brenobn <brenobragabn@gmail.com>
2024-02-08 14:16:21 -0300

    Importing remainder of dependencies to render products that are in the bag

--- src/app/cart.tsx
+++ src/app/cart.tsx
@@ -1,10 +1,23 @@
-import { Header } from "@/components/header"
 import { View } from "react-native"
 
+import { useCartStore } from "@/stores/cart-store"
+
+import { Header } from "@/components/header"
+import { Product } from "@/components/product"
+
 export default function Cart(){
+  const cartStore = useCartStore()
+   
   return(
     <View className="flex-1 pt-8">
       <Header title="Seu carrinho" />
+
+      <View className="p-5 flex-1">
+        {cartStore.products.map((product) => (
+            <Product key={product.id} data={product} />
+          ))
+        }
+      </View>
     </View>
   )
 }
